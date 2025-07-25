0e9c7c839c7e17906226f210476966542d49bf95
Raí Salomão <raisalomaomangueira@gmail.com>
2025-07-25 20:18:40 -0300

    mudancas na classe de operacoes, conta, repository

--- bank-api/src/main/java/ifba/edu/br/bank_api/repositories/OperacaoRepository.java
+++ bank-api/src/main/java/ifba/edu/br/bank_api/repositories/OperacaoRepository.java
@@ -1,9 +1,15 @@
 package ifba.edu.br.bank_api.repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import ifba.edu.br.bank_api.models.Conta;
 import ifba.edu.br.bank_api.models.Operacao;
+import ifba.edu.br.bank_api.models.TipoOperacao;
 
 public interface OperacaoRepository extends JpaRepository<Operacao, Long> {
-    
+    List<Operacao> findByContaAndTipoAndDataBetween(Conta conta, TipoOperacao tipo, LocalDateTime inicio, LocalDateTime fim);
+    List<Operacao> findByContaAndDataBetween(Conta conta, LocalDateTime inicio, LocalDateTime fim);
 }
