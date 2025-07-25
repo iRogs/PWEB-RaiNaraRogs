94ec1216a1b75b32c06b26857369ffc227fd0b59
IainaraCerqueira <cerqueiranara10@gmail.com>
2025-07-25 20:51:13 -0300

    Correções no Operação Service e UsuarioService

--- bank-api/src/main/java/ifba/edu/br/bank_api/services/OperacaoService.java
+++ bank-api/src/main/java/ifba/edu/br/bank_api/services/OperacaoService.java
@@ -17,9 +17,18 @@ import java.util.Optional;
 @Service
 public class OperacaoService {
 
-    private final ContaRepository accountRepository;
+    private final ContaRepository contaRepository;
     private final OperacaoRepository operacaoRepository;
     private final EmailService emailService;
+    
+
+   
+
+    public OperacaoService(ContaRepository contaRepository, OperacaoRepository operacaoRepository) {
+        this.contaRepository = contaRepository;
+        this.operacaoRepository = operacaoRepository;
+        this.emailService = emailService;
+    }
 
     private void salvarOperacao(Conta conta, TipoOperacao tipo, BigDecimal valor, String descricao) {
         Operacao operacao = new Operacao();
@@ -38,7 +47,7 @@ public class OperacaoService {
 
         Conta conta = findAccount(accountId);
         conta.setSaldo(conta.getSaldo().add(amount));
-        accountRepository.save(conta);
+        contaRepository.save(conta);
 
         salvarOperacao(conta, TipoOperacao.DEPOSITO, amount, "Depósito");
         enviarEmail(conta, "Depósito", amount);
@@ -57,7 +66,7 @@ public class OperacaoService {
         }
 
         conta.setSaldo(conta.getSaldo().subtract(amount));
-        accountRepository.save(conta);
+        contaRepository.save(conta);
 
         salvarOperacao(conta, TipoOperacao.SAQUE, amount, "Saque");
         enviarEmail(conta, "Saque", amount);
@@ -77,7 +86,7 @@ public class OperacaoService {
         }
 
         conta.setSaldo(conta.getSaldo().subtract(amount));
-        accountRepository.save(conta);
+        contaRepository.save(conta);
         salvarOperacao(conta, TipoOperacao.PAGAMENTO, amount, descricao);
         enviarEmail(conta, "Pagamento: " + descricao, amount);
 
@@ -95,7 +104,7 @@ public class OperacaoService {
     }
 
     private Conta findAccount(Long accountId) {
-        return accountRepository.findById(accountId)
+        return contaRepository.findById(accountId)
                 .orElseThrow(() -> new IllegalArgumentException("Conta não encontrada."));
     }
 
