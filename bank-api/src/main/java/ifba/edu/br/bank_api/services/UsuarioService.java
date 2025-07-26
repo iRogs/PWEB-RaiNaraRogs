46405d7e8d5e465a6037190b3fb6cb338dc01dff
IainaraCerqueira <cerqueiranara10@gmail.com>
2025-07-26 00:55:26 -0300

    correções nas classes UsuarioService, OperacaoService, TokenDTO, Operacao e Usuario

--- bank-api/src/main/java/ifba/edu/br/bank_api/services/UsuarioService.java
+++ bank-api/src/main/java/ifba/edu/br/bank_api/services/UsuarioService.java
@@ -38,12 +38,13 @@ public class UsuarioService {
 
     @Transactional
     public UsuarioDTO cadastrar(UsuarioForm form){
-        String senhaHash = new BCryptPasswordEncoder().encode(form.senha());
+        String senhaHash = passwordEncoder.encode(form.senha());
         Conta conta = new Conta();
         conta.setAgencia("0001");
         conta.setSaldo(BigDecimal.ZERO);
         contaRepository.save(conta);
         Usuario novoUsuario = new Usuario(form.nome(), form.email(), form.cpf(), senhaHash, conta, LocalDateTime.now());
+        conta.setUsuario(novoUsuario); 
         usuarioRepository.save(novoUsuario);
 
         enviarEmailBoasVindas(novoUsuario);
